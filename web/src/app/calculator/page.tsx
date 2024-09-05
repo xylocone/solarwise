d2a29b96aecb9fd29bc64a25b1749172ecd8d167
Deepanshu <deepanshu.9dd@gmail.com>
2024-09-05 17:41:56 +0530

    add calculator page

---
+++ web/src/app/calculator/page.tsx
@@ -0,0 +1,9 @@
+import Map from "@/components/Map";
+
+export default function Calculator() {
+    return (
+        <div className="flex w-screen h-[70vh] bg-gray-300 justify-center items-center">
+            <Map />
+        </div>
+    );
+}
