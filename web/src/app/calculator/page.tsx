5c44fa678355f50f6d6f5e4733906afb56d37bb8
Deepanshu <deepanshu.9dd@gmail.com>
2024-09-05 18:35:07 +0530

    add header

--- web/src/app/calculator/page.tsx
+++ web/src/app/calculator/page.tsx
@@ -2,7 +2,10 @@ import Map from "@/components/Map";
 
 export default function Calculator() {
     return (
-        <div className="flex w-screen h-[70vh] bg-gray-300 justify-center items-center">
+        <div className="flex w-screen h-[80vh] bg-gray-300 justify-center items-center flex-col">
+            <h3 className="text-5xl black font-extrabold my-5">
+                Now Darg your exact location
+            </h3>
             <Map />
         </div>
     );
